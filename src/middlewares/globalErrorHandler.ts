87d448882d2d3f924e2faf6c52f9da1d3c19dab8
mobinulislam143 <mobinulislammahi@gmail.com>
2025-08-18 19:39:53 +0600

    delete server data.

--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -1,7 +1,7 @@
 import { Prisma } from "@prisma/client";
 import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status";
-import { ZodError } from "zod";
+import {ZodError}  from "zod";
 import handleZodError from "../errors/handleZodError";
 import ApiError from "../errors/ApiError";
 import parsePrismaValidationError from "../errors/parsePrismaValidationError";
